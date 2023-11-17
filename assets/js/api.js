00c83d1d0bb13040fdf2e8d60fe5fcd835f4d9eb
Likheet Shetty <likheet.s@gmail.com>
2023-11-17 20:24:38 +0530

    Contents

---
+++ assets/js/api.js
@@ -0,0 +1,29 @@
+'use strict';
+const api_key = 'dcff675e961a3a1b82c590f81cfe12bf';
+
+export const fetchData = (URL,callback)=>{
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res=>res.json())
+    .then(data=>callback(data))
+}
+
+export const url ={
+    currentWeather(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat,lon){
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query search query e.g. :"london" , "New Yourk"  
+     */
+    geo(query){
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
---
+++ assets/js/route.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import {updateWeather , error404} from "./app.js"
+const defaultLoaction="#/weather?lat=51.5073219&lon=-0.1276474";
+const approximatelocation = async () => {
+    try {
+        let ipResponse = await fetch("https://api.ipify.org/?format=json");
+        let ipData = await ipResponse.json();
+        let ip = ipData.ip;
+        let locationResponse = await fetch(`https://ipinfo.io/${ip}?token=c177813f87d9fa`);
+        let locationData = await locationResponse.json();
+        const[latitude, longitude] = locationData.loc.split(",");
+        updateWeather(`lat=${latitude}`,`lon=${longitude}`)
+        window.location.hash=`#/approximatelocation`;
+    } catch (error) {
+        console.error("Error fetching location data:", error);
+    }
+};
+const currentLocation = function(){
+    window.navigator.geolocation.getCurrentPosition(res=>{
+        const{latitude,longitude}=res.coords;
+        updateWeather(`lat=${latitude}`,`lon=${longitude}`)
+    },err=>{
+        window.location.hash = "#/approximatelocation";
+    })
+}
+const searchedLocation = query=>updateWeather(...query.split("&"));
+const routes = new Map([
+    ["/current-location",currentLocation],
+    ["/weather",searchedLocation],
+    ["/approximatelocation",approximatelocation]
+]);
+const checkHash = function(){
+    const requestURL = window.location.hash.slice(1);
+    const [route , query] = requestURL.includes ? requestURL.split("?") : [requestURL];
+    routes.get(route) ? routes.get(route)(query) : error404();
+}
+window.addEventListener("hashchange",checkHash)
+window.addEventListener("load",function(){
+    if(!window.location.hash)
+        window.location.hash="#/current-location";
+    else
+        checkHash();
+})
